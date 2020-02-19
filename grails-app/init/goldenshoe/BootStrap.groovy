f6be6ac63fc57fbca3c204b84556b7c86de6e2b9
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-02-19 12:47:00 +0000

    Initialised Product objects to be added to H2 database upon startup

--- grails-app/init/goldenshoe/BootStrap.groovy
+++ grails-app/init/goldenshoe/BootStrap.groovy
@@ -3,7 +3,52 @@ package goldenshoe
 class BootStrap {
 
     def init = { servletContext ->
+
+        def yeezy = new Product(
+                productName : "Yeezys",
+                price : 500.50,
+                description : "Kanye West's new Yeezy boots from his new collection.",
+                stock : 100 ,
+                colour : "grey",
+                availableSizes : [8, 9, 10],
+                type : "stylish",
+                mainImgUrl : "YEEZYmainImgUrl",
+                secondImgUrl : "YEEZYsecondImgUrl",
+                thirdImgUrl : "YEEZYthirdImgUrl"
+        ).save()
+
+        def heels = new Product(
+                productName : "Heel Shoes",
+                price : 44.99,
+                description : "High heel shoes imported directly from Italy.",
+                stock : 20 ,
+                colour : "Red",
+                availableSizes : [5, 7, 8],
+                type : "heels",
+                mainImgUrl : "HEELSmainImgUrl",
+                secondImgUrl : "HEELSsecondImgUrl",
+                thirdImgUrl : "HEELSthirdImgUrl"
+        ).save()
+
+        def adidas = new Product(
+                productName : "Adidas Classics",
+                price : 70.00,
+                description : "New Adidas classic black running shoes frm their sports collection.",
+                stock : 75 ,
+                colour : "Black",
+                availableSizes : [7, 9, 11],
+                type : "trainers",
+                mainImgUrl : "TRAINERSmainImgUrl",
+                secondImgUrl : "TRAINERSsecondImgUrl",
+                thirdImgUrl : "TRAINERSthirdImgUrl"
+        ).save()
+
+
+
     }
+
     def destroy = {
+
     }
+
 }
