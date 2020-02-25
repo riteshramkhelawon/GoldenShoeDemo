c0dc492a769e927c12b0474585899943fecce100
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-02-25 15:27:04 +0000

    Removed bootstrap init objects

--- grails-app/init/goldenshoe/BootStrap.groovy
+++ grails-app/init/goldenshoe/BootStrap.groovy
@@ -5,57 +5,57 @@ class BootStrap {
     def init = { servletContext ->
 
 
-        def yeezys = new Product(
-                productName : "Yeezys",
-                price : 500.50,
-                shortDescription : "Kanye West's new Yeezy boots from his new collection.",
-                descriptionPoints: ["New line of Yeezys", "Flexible material to fit the shape of your foot",
-                                    "Air breathing material", "Air bubble to refresh your feet"],
-                stock : 100 ,
-                colour : "Grey",
-                availableSizes : [8, 9, 10],
-                type : "stylish",
-                mainImgUrl : "/assets/shoes/yeezy-main.jpg",
-                secondImgUrl : "/assets/shoes/yeezy-second.jpg",
-                thirdImgUrl : "/assets/shoes/yeezy-third.jpg",
-                videoUrl: "https://www.youtube.com/embed/ruYWz8L2IcQ"
-        ).save()
-
-        def heels = new Product(
-                productName : "Heels",
-                price : 44.99,
-                shortDescription : "High heel leather shoes. Manufactured by a major fashion company in Italy",
-                descriptionPoints: ["New line of heels", "Extra high heels to give you an extra height boost",
-                                    "New heel material for more strength", "Amazing gloss finish"],
-                stock : 20 ,
-                colour : "Red",
-                availableSizes : [5, 7, 8],
-                type : "heels",
-                mainImgUrl : "/assets/shoes/heels-main.jpg",
-                secondImgUrl : "/assets/shoes/heels-second.jpg",
-                thirdImgUrl : "/assets/shoes/heels-third.jpg",
-                videoUrl: "https://www.youtube.com/embed/u6jKPojkhmY"
-        ).save()
-
-        def adidas = new Product(
-                productName : "Adidas",
-                price : 70.00,
-                shortDescription : "New Adidas classic white running shoes from their sports collection.",
-                descriptionPoints: ["New line of Adidas running shoes", "Flexible material to fit the shape of your foot",
-                                    "Air breathing material, great for running", "Air bubble to refresh your feet"],
-                stock : 75 ,
-                colour : "White",
-                availableSizes : [7, 9, 11],
-                type : "trainers",
-                mainImgUrl : "/assets/shoes/adidas-main.jpg",
-                secondImgUrl : "/assets/shoes/adidas-second.jpg",
-                thirdImgUrl : "/assets/shoes/adidas-third.jpg",
-                videoUrl: "https://www.youtube.com/embed/_sIaPgpM2v0"
-        ).save()
+//        def yeezys = new Product(
+//                productName : "Yeezys",
+//                price : 500.50,
+//                shortDescription : "Kanye West's new Yeezy boots from his new collection.",
+//                descriptionPoints: ["New line of Yeezys", "Flexible material to fit the shape of your foot",
+//                                    "Air breathing material", "Air bubble to refresh your feet"],
+//                stock : 100 ,
+//                colour : "Grey",
+//                availableSizes : [8, 9, 10],
+//                type : "stylish",
+//                mainImgUrl : "/assets/shoes/yeezy-main.jpg",
+//                secondImgUrl : "/assets/shoes/yeezy-second.jpg",
+//                thirdImgUrl : "/assets/shoes/yeezy-third.jpg",
+//                videoUrl: "https://www.youtube.com/embed/ruYWz8L2IcQ"
+//        ).save()
+//
+//        def heels = new Product(
+//                productName : "Heels",
+//                price : 44.99,
+//                shortDescription : "High heel leather shoes. Manufactured by a major fashion company in Italy",
+//                descriptionPoints: ["New line of heels", "Extra high heels to give you an extra height boost",
+//                                    "New heel material for more strength", "Amazing gloss finish"],
+//                stock : 20 ,
+//                colour : "Red",
+//                availableSizes : [5, 7, 8],
+//                type : "heels",
+//                mainImgUrl : "/assets/shoes/heels-main.jpg",
+//                secondImgUrl : "/assets/shoes/heels-second.jpg",
+//                thirdImgUrl : "/assets/shoes/heels-third.jpg",
+//                videoUrl: "https://www.youtube.com/embed/u6jKPojkhmY"
+//        ).save()
+//
+//        def adidas = new Product(
+//                productName : "Adidas",
+//                price : 70.00,
+//                shortDescription : "New Adidas classic white running shoes from their sports collection.",
+//                descriptionPoints: ["New line of Adidas running shoes", "Flexible material to fit the shape of your foot",
+//                                    "Air breathing material, great for running", "Air bubble to refresh your feet"],
+//                stock : 75 ,
+//                colour : "White",
+//                availableSizes : [7, 9, 11],
+//                type : "trainers",
+//                mainImgUrl : "/assets/shoes/adidas-main.jpg",
+//                secondImgUrl : "/assets/shoes/adidas-second.jpg",
+//                thirdImgUrl : "/assets/shoes/adidas-third.jpg",
+//                videoUrl: "https://www.youtube.com/embed/_sIaPgpM2v0"
+//        ).save()
 
 
 //
-//        def newOrder = new OrdersMade(
+//        def newOrder = new CustomerOrder(
 //                customer: "Ritesh Ramkhelawon",
 //                orderItems: [adidas, heels],
 //                orderNumber: "abc123"
